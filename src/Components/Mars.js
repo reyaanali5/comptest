7f2cf4eee7ca38b690e8cd933dd2cc05ffc279d6
asra-r <asra.ramlawie@students.mq.edu.au>
2023-10-09 21:33:17 +1100

    API for all

--- src/Components/Mars.js
+++ src/Components/Mars.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Mars() {
+  const [planetData, setPlanetData] = useState(null);
+
+  useEffect(() => {
+    const apiKey = '3J+dWBW8QXneboHS6ry8Gg==KreuAlFtf9xLOw1V';
+    
+    const apiUrl = 'https://api.api-ninjas.com/v1/planets?name=Mars';
+
+    axios.get(apiUrl, {
+      headers: {
+        'X-Api-Key': apiKey,
+      },
+    })
+    .then((response) => {
+      setPlanetData(response.data[0]); 
+    })
+    .catch((error) => {
+      console.error('API Error:', error);
+    });
+  }, []);
+
   return (
     <div>
       <div className="planet-heading">
-        <h2>Mars</h2>
+        <h2>MARS</h2>
+      </div>
+      <div className="planet-info">
+        {planetData && (
+          <div>
+            <p>Mass: {planetData.mass} Jupiters</p>
+            <p>Radius: {planetData.radius} Jupiters</p>
+            <p>Orbital Period: {planetData.period} Earth days</p>
+            <p>Temperature: {planetData.temperature} Kelvin</p>
+          </div>
+        )}
       </div>
     </div>
   );
