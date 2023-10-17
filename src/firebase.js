ab55d7b98915ec59c9c8dfdff1ed08e35eab0ed8
Ann Latif <anastasialatif@hotmail.com>
2023-10-17 14:02:07 +1100

    firebase issue

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { onAuthStateChanged } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
